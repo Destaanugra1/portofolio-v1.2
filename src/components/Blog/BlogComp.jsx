40cc83369ad43d0e0d5f39cdad07591c064cb941
dede11 <destaanugrapratama@gmail.com>
2024-07-06 20:12:22 +0700

    animasi gerak

--- src/components/Blog/BlogComp.jsx
+++ src/components/Blog/BlogComp.jsx
@@ -1,13 +1,17 @@
 import Styles from "./BlogComp.module.css";
 import images from "../images/kageno.png";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 const BlogComp = () => {
   return (
     <div id="Blog" className={Styles.blog}>
-      <div>
+      <div data-aos="fade-right" data-aos-delay="400">
         <img className={Styles.gambar} src={images} alt="foto" />
       </div>
-      <div>
+      <div data-aos="fade-left" data-aos-delay="500"> 
         <h1 className={Styles.intro}>Hallo Saya Akan Memperkenalkan Diri</h1>
         <div className={Styles.perkenalan}>
           <h3 className={Styles.perkenalan__diri}>Nama:</h3>
